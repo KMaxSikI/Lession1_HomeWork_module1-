57cc4fcec7fab8685184505b4d86a19051ccc5e9
KMaxSikI <113554568+KMaxSikI@users.noreply.github.com>
2022-09-18 21:45:06 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,14 @@
+#Модуль 1
+#Сотрудник
+# Eровень ретинга в компании не меньше 5
+
+name = 'Sergey' #Имя
+age = 35 #Возрвст
+office = 526 #Рабочий кабинет
+rating = 8.5 #средний рейтинг в компании
+
+print(name, type(name))
+print(age, type(age))
+print(office, type(office))
+print(rating, type(rating))
+print(rating > 5, type(rating > 5))
